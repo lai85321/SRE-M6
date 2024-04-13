5fc284ae1927134db35316ca7f44e23682bb9bbc
tina.lei <lai85321@gmail.com>
2024-04-13 19:45:43 +0800

    [feat] add pre-commit

--- src/controller.js
+++ src/controller.js
@@ -3,11 +3,12 @@ const getAddOne = async (req, res) => {
   let input = parseInt(number);
   try {
     if (input === null || isNaN(input)) {
-      return res.status(400).json({ error: "Please enter a number" });
+      return res.status(400).json({ error: "Please enter a number!" });
     }
     let result = input + 1;
     res.status(200).json({ result: result });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Internal server error" });
   }
 };
